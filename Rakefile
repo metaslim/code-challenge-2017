aa51ae21f905822c252495daa4a7da7d474c644b
wsalim <wsalim@booster.com>
2017-03-24 23:41:03 +0800

    use bin/survey-tool instead

--- Rakefile
+++ Rakefile
@@ -1,62 +1,8 @@
-require 'optparse'
-require 'rake/testtask'
 require 'rspec/core/rake_task'
 
-task :default => [:run]
-
-desc "load the survey and the answer and execute it all!"
-task "run" do
-  $LOAD_PATH.unshift(File.dirname(__FILE__), "lib")
-  require 'application'
-
-  ARGV.shift
-  ARGV.shift
-
-  ARGV << '-h' if ARGV.empty?
-
-	options = {}
-	survey_file = 'example-data/survey-2.csv'
-  response_file = 'example-data/survey-2-responses.csv'
-
-	# parse param for rake
-	opt_parser = OptionParser.new do |opt|
-		opt.banner = "Usage: \n\n To run test : rake spec \n\n To run the app : rake run -- [options] \n\n For eg. rake run -- -s=#{survey_file} -r=#{response_file}"
-		opt.on("-s CSV FILE", "--survey_file CSV FILE") do |csv_file| 
-			csv_file.gsub! /\=/,''
-			options[:survey_file] = csv_file 
-		end
-
-  	opt.on("-r CSV FILE", "--response_file CSV FILE") do |csv_file|
-  		csv_file.gsub! /\=/,''
-  		options[:response_file] = csv_file 
-  	end
-
-	  opt.on_tail("-h", "--help", "help") do
-	   	puts opt_parser
-	    exit
-	  end
-	end
-
-  opt_parser.parse!
-
-  # load the data files into strings for you
-  responses_csv = File.read(options[:response_file].to_s)
-  survey_csv = File.read(options[:survey_file].to_s)
-
-  # call the app, passing the data as strings containing CSV
-  Application.cli(responses_csv, survey_csv)
-end
-
-
+task :default => [:spec]
 
 RSpec::Core::RakeTask.new(:spec) do |t|
 	t.pattern = Dir.glob('spec/**/*_spec.rb')
 	t.rspec_opts = '--format documentation --color'
-end
-
-desc "Run Tests"
-Rake::TestTask.new(:test) do |test|
-  test.libs << 'test'
-
-  test.test_files = FileList['test/**/*_test.rb']
 end
--- lib/application.rb
+++ lib/application.rb
@@ -1,24 +1,58 @@
 require_relative 'loader/loader_pack'
 require_relative 'presenter/presenter_pack'
 require_relative 'report/report'
+require 'optparse'
 
 class Application
+	def cli
+		(responses_csv, survey_csv) = parse_cli_arguments
+	  run(responses_csv, survey_csv)
+	end
 
-	class << self
-		def cli(response_csv, survey_csv)
-    	# Your implementation probably starts here...
-			questions = QuestionsLoader.load_from(survey_csv)
-			responses = ResponsesLoader.load_from(response_csv)
+	private
+	def parse_cli_arguments
+	  ARGV << '-h' if ARGV.empty?
 
-			report = Report.new(questions, responses)
-			
-			report
-			.add_presenter(ParticipationPercentage.new)
-			.add_presenter(TotalParticipant.new)
-			.add_presenter(RatingAverage.new)
-			.add_presenter(SingleSelection.new)
+		options = {}
+		survey_file = 'example-data/survey-2.csv'
+	  response_file = 'example-data/survey-2-responses.csv'
 
-			puts report.build
-  	end
+		# parse param for rake
+		opt_parser = OptionParser.new do |opt|
+			opt.banner = "Usage: \n\n survey-tool [options] \n\n For eg. survey-tool -s=#{survey_file} -r=#{response_file}"
+			opt.on("-s CSV FILE", "--survey_file CSV FILE") do |csv_file| 
+				csv_file.gsub! /\=/,''
+				options[:survey_file] = csv_file 
+			end
+
+	  	opt.on("-r CSV FILE", "--response_file CSV FILE") do |csv_file|
+	  		csv_file.gsub! /\=/,''
+	  		options[:response_file] = csv_file 
+	  	end
+
+		  opt.on_tail("-h", "--help", "help") do
+		   	puts opt_parser
+		    exit
+		  end
+		end
+
+	  opt_parser.parse!
+	  return options[:response_file], options[:survey_file]
+	end
+	
+	def run(response_csv, survey_csv)
+  	# load the data files into strings for you
+		questions = QuestionsLoader.load_from(File.read(survey_csv))
+		responses = ResponsesLoader.load_from(File.read(response_csv))
+
+		report = Report.new(questions, responses)
+		
+		report
+		.add_presenter(ParticipationPercentage.new)
+		.add_presenter(TotalParticipant.new)
+		.add_presenter(RatingAverage.new)
+		.add_presenter(SingleSelection.new)
+
+		puts report.build
 	end
 end
