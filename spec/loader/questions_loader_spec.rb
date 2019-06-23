123bbc76df8f056ce1015b81264405f727726912
metaslim <welly@MacBook-Pro.local>
2019-06-23 20:47:43 +1000

    fix test

--- spec/loader/questions_loader_spec.rb
+++ spec/loader/questions_loader_spec.rb
@@ -5,11 +5,11 @@ describe "questions_loader" do
   let(:survey_csv) {
     <<-CSV
       theme,type,text
-      The Work,ratingquestion,I like the kind of work I do.
-      The Work,ratingquestion,"In general, I have the resources (e.g., business tools, information, facilities, IT or functional support) I need to be effective."
-      The Work,ratingquestion,We are working at the right pace to meet our goals.
-      The Work,ratingquestion,I feel empowered to get the work done for which I am responsible.
-      The Work,singleselect,Manager
+The Work,ratingquestion,I like the kind of work I do.
+The Work,ratingquestion,"In general, I have the resources (e.g., business tools, information, facilities, IT or functional support) I need to be effective."
+The Work,ratingquestion,We are working at the right pace to meet our goals.
+The Work,ratingquestion,I feel empowered to get the work done for which I am responsible.
+The Work,singleselect,Manager
     CSV
   }
 
@@ -22,14 +22,14 @@ describe "questions_loader" do
 
     it "has correct theme, type, text" do
       first_question = questions.first
-      expect(first_question.theme.strip).to eq('The Work')
+      expect(first_question.theme).to eq('The Work')
       expect(first_question.type).to eq('ratingquestion')
       expect(first_question.text).to eq('I like the kind of work I do.')
     end
 
     it "has single select question as last question" do
       last_question = questions.last
-      expect(last_question.theme.strip).to eq('The Work')
+      expect(last_question.theme).to eq('The Work')
       expect(last_question.type).to eq('singleselect')
       expect(last_question.text).to eq('Manager')
     end
